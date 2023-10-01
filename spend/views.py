10f819814c189cdaa753863f83eeb79f15e95ff3
Dmytro Shvets <dirolius@gmail.com>
2023-10-01 22:27:41 +0300

    Updated the processing of the revenuestatistic__revenue field in the spend_statistics view

--- spend/views.py
+++ spend/views.py
@@ -1,20 +1,27 @@
+from django.db import models
 from django.shortcuts import render
 
-from django.db.models import Sum
+from django.db.models import Sum, Subquery, OuterRef
 from django.http import JsonResponse
 from django.views.decorators.http import require_safe
 
+from revenue.models import RevenueStatistic
 from .models import SpendStatistic
 
 
 @require_safe
 def spend_statistics(request):
-    spend_data = SpendStatistic.objects.values("date", "name").annotate(
+    subquery = RevenueStatistic.objects.filter(spend=OuterRef('pk')).values('spend').annotate(
+        total_revenue=Sum('revenue')
+    ).values('total_revenue')[:1]
+
+    spend_data = SpendStatistic.objects.annotate(
+        total_revenue=Subquery(subquery, output_field=models.DecimalField(max_digits=9, decimal_places=2))
+    ).values("date", "name").annotate(
         total_spend=Sum("spend"),
         total_impressions=Sum("impressions"),
         total_clicks=Sum("clicks"),
         total_conversion=Sum("conversion"),
-        total_revenue=Sum("revenuestatistic__revenue"),
     )
 
     return JsonResponse({"spend_data": list(spend_data)})
